8c001d09004644425383f65a2c09d8b197e32a53
Adrian Lee <leeadrian841@gmail.com>
2016-10-12 23:08:03 +0800

    amend erros and add new edits

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 // import express
-var express = require('express')
 // import bodyParser object
+var express = require('express')
 var bodyParser = require('body-parser')
+var methodOverride = require('method-override')
 
 // instantiating the express server
 var app = express()
@@ -12,8 +13,19 @@ var teachers_routes = require('./routes/teachers')
 var students_routes = require('./routes/students')
 
 // capture all requests, let it filtered by body-parse package so as to be json objects
+app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: false
+}))
+
+// run methodOverride for all requests
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
 }))
 
 // the view engine for express is ejs. HENCE, res.render(index) => index.ejs inside VIEWS FOLDER
